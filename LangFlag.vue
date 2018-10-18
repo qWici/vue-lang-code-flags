d8b03df048591876c11a569817795137d18af0e8
qWici <ivk@elementalsweb.com>
2018-10-18 18:45:19 +0300

    Init project

---
+++ LangFlag.vue
@@ -0,0 +1,20 @@
+<template>
+    <span v-if="iso" class="flag-icon" :class="flagIconClass" :title="title || iso"></span>
+</template>
+<script>
+  import 'flag-icon-css/css/flag-icon.css'
+
+  export default {
+    name: 'LangFlag',
+    props: {
+      iso: {type: String, default: null},
+      title: {type: String, default: null},
+      squared: {type: Boolean, default: true},
+    },
+    computed: {
+      flagIconClass: function () {
+        return ((!!this.squared) ? 'flag-icon-squared ' : '') + 'flag-icon-' + this.iso.toLowerCase();
+      }
+    }
+  }
+</script>
