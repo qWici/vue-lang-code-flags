d3a3a931cd20a3bdf5b2c72fe51ce1894d5415d3
Ivan Kucher <dev.kucher@gmail.com>
2019-05-10 17:36:09 +0300

    Fix lint

--- LangFlag.vue
+++ LangFlag.vue
@@ -1,17 +1,19 @@
-<template>
+<template>12
     <span v-if="iso" class="flag-icon" :class="flagIconClass" :title="title || iso"></span>
 </template>
 <script>
-  import "flag-icon-css/css/flag-icon.css";
-  import relation from "./relation";
+import "flag-icon-css/css/flag-icon.css";
+import relation from "./relation";
 
-  export default {
+export default {
     name: "LangFlag",
+
     props: {
-      iso: {type: String, default: null, required: true},
-      title: {type: String, default: null},
-      squared: {type: Boolean, default: true}
+      iso: { type: String, required: true },
+      title: { type: String, default: null },
+      squared: { type: Boolean, default: true }
     },
+
     computed: {
       flagIconClass: function () {
         return (
@@ -24,5 +26,5 @@
         return relation[this.iso.toLowerCase()];
       }
     }
-  };
+};
 </script>
